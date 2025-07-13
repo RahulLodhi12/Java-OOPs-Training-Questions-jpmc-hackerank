b82cef64c52bbd82803a0e7d82bb89422e0fcaec
Rahul Lodhi <rahullodhimts@gmail.com>
2025-07-13 21:39:22 +0530

    night 13/7/2025

--- src/AuthenticationSolution.java
+++ src/AuthenticationSolution.java
@@ -43,52 +43,52 @@ class User implements IUser{
 
     @Override
     public int getId() {
-        return 0;
+        return id;
     }
 
     @Override
     public void setId(int id) {
-
+        this.id = id;
     }
 
     @Override
     public String getEmail() {
-        return null;
+        return email;
     }
 
     @Override
     public void setEmail(String email) {
-
+        this.email = email;
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return password;
     }
 
     @Override
     public void setPassword(String password) {
-
+        this.password = password;
     }
 
     @Override
     public int getIncorrectAttempt() {
-        return 0;
+        return incorrectAttempt;
     }
 
     @Override
     public void setIncorrectAttempt(int incorrectAttempt) {
-
+        this.incorrectAttempt=incorrectAttempt;
     }
 
     @Override
     public String getLocation() {
-        return null;
+        return location;
     }
 
     @Override
     public void setLocation(String location) {
-
+        this.location = location;
     }
 }
 
@@ -98,66 +98,127 @@ class ApplicationAuthState implements IApplicationAuthState{
     List<IUser> usersLoggedIn;
 
     public ApplicationAuthState(List<String> allowedLocations){
-        this.allowedLocations = allowedLocations;
+        this.allowedLocations = new ArrayList<>(allowedLocations);
         registeredUsers = new ArrayList<>();
         usersLoggedIn = new ArrayList<>();
     }
 
     @Override
     public List<IUser> getRegisteredUser() {
-        return null;
+        return registeredUsers;
     }
 
     @Override
     public void setRegisteredUser(List<IUser> registeredUsers) {
-
+        this.registeredUsers = registeredUsers;
     }
 
     @Override
     public List<IUser> getUsersLoggedIn() {
-        return null;
+        return usersLoggedIn;
     }
 
     @Override
     public void setUsersLoggedIn(List<IUser> usersLoggedIn) {
-
+        this.usersLoggedIn = usersLoggedIn;
     }
 
     @Override
     public List<String> getAllowedLocations() {
-        return null;
+        return allowedLocations;
     }
 
     @Override
     public void setAllowedLocations(List<String> allowedLocations) {
-
+        this.allowedLocations = allowedLocations;
     }
 
     @Override
     public String Register(IUser user) {
-        if(registeredUsers.contains(user)){
-            return "Already Register";
+        for(IUser iUser: registeredUsers){
+            if(iUser.getEmail().equals(user.getEmail())) {
+                return user.getEmail() + " is already registered!";
+            }
         }
-        else{
-            registeredUsers.add(user);
-            return "Successfully Register";
-        }
+
+        registeredUsers.add(user);
+        return user.getEmail() + " registered successfully!";
     }
 
     @Override
     public String Login(IUser user) {
-        if(registeredUsers.contains(user) && user.getIncorrectAttempt()<3){
-
+        IUser myuser=null;
+        for(IUser iUser: registeredUsers){
+            if(iUser.getEmail().equals(user.getEmail())){
+                myuser=iUser;
+                break;
+            }
+        }
+        if(myuser==null){
+            return user.getEmail() + " is not registered!";
         }
 
-        return "";
+        if(myuser.getIncorrectAttempt()>=3){
+            return myuser.getEmail() + " is blocked!";
+        }
+
+        if(!myuser.getPassword().equals(user.getPassword())){
+            myuser.setIncorrectAttempt(myuser.getIncorrectAttempt()+1);
+            return myuser.getEmail() + " password is incorrect!";
+        }
+
+        for(IUser iUser: usersLoggedIn){
+            if(myuser.getEmail().equals(iUser.getEmail())){
+                return myuser.getEmail() + " is already logged in!";
+            }
+        }
+
+        usersLoggedIn.add(myuser);
+        myuser.setIncorrectAttempt(0);
+        return myuser.getEmail() + " logged in successfully!";
     }
 
     @Override
     public String Logout(IUser user) {
-        return null;
+        IUser myuser=null;
+        for(IUser iUser: usersLoggedIn){
+            if(iUser.getEmail().equals(user.getEmail())){
+                myuser=iUser;
+                break;
+            }
+        }
+
+        if(myuser==null){
+            return user.getEmail() + " is not logged in!";
+        }
+
+        usersLoggedIn.remove(myuser);
+        return myuser.getEmail() + " logged out successfully!";
     }
 }
+
+/*
+I/P:
+1
+location4
+4
+16,user16@email.com,14165,location16
+8,user8@email.com,89680,location8
+17,user17@email.com,26883,location17
+16,user16@email.com,36862,location16
+10
+Register:2
+Register:2
+Login:1
+Register:2
+Login:0
+Login:0
+Login:0
+Register:2
+Register:1
+Register:2
+*/
+
 
 public class AuthenticationSolution {
 
@@ -196,5 +257,40 @@ public class AuthenticationSolution {
         }
 
         writer.close();
+
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        PrintWriter writer = new PrintWriter(System.out);
+//
+//        List<IUser> users = new ArrayList<IUser>();
+//        List<String> allowedLocations = new ArrayList<String>();
+//
+//        int allowedLocationCount = Integer.parseInt(bufferedReader.readLine().trim());
+//        for (int i = 0; i < allowedLocationCount; i++) {
+//            String a = bufferedReader.readLine().trim();
+//            allowedLocations.add(a);
+//        }
+//
+//        ApplicationAuthState authState = new ApplicationAuthState(allowedLocations);
+//
+//        int usersCount = Integer.parseInt(bufferedReader.readLine().trim());
+//        for (int i = 0; i < usersCount; i++) {
+//            String[] a = bufferedReader.readLine().trim().split(",");
+//            users.add(new User(Integer.parseInt(a[0]), a[1], a[2], a[3]));
+//        }
+//
+//        int actionCount = Integer.parseInt(bufferedReader.readLine().trim());
+//        for (int i = 0; i < actionCount; i++) {
+//            String[] a = bufferedReader.readLine().trim().split(":");
+//            int uindex = Integer.parseInt(a[1]);
+//            if (a[0].equals("Register")) {
+//                writer.println(authState.Register(users.get(uindex)));
+//            } else if (a[0].equals("Login")) {
+//                writer.println(authState.Login(users.get(uindex)));
+//            } else if (a[0].equals("Logout")) {
+//                writer.println(authState.Logout(users.get(uindex)));
+//            }
+//        }
+//
+//        writer.close();
     }
 }
