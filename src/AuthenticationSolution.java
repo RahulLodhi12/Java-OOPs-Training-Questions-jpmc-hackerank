394402fedcca441b6cfef7a35a5ac1561c9f11e2
Rahul Lodhi <rahullodhimts@gmail.com>
2025-07-14 08:20:15 +0530

    morning 14/7/25

--- src/AuthenticationSolution.java
+++ src/AuthenticationSolution.java
@@ -147,7 +147,7 @@ class ApplicationAuthState implements IApplicationAuthState{
 
     @Override
     public String Login(IUser user) {
-        IUser myuser=null;
+        IUser myuser=null; //registered user
         for(IUser iUser: registeredUsers){
             if(iUser.getEmail().equals(user.getEmail())){
                 myuser=iUser;
@@ -167,15 +167,25 @@ class ApplicationAuthState implements IApplicationAuthState{
             return myuser.getEmail() + " password is incorrect!";
         }
 
+        if(!allowedLocations.contains(myuser.getLocation())){
+            return user.getEmail() + " is not allowed to login from this location!";
+        }
+
         for(IUser iUser: usersLoggedIn){
-            if(myuser.getEmail().equals(iUser.getEmail())){
-                return myuser.getEmail() + " is already logged in!";
+            if(user.getEmail().equals(iUser.getEmail())){
+                if(user.getLocation().equals(iUser.getLocation())){
+                    return user.getEmail() + " is already logged in!";
+                }
+                else{
+                    return user.getEmail() + " is already logged in from another location!";
+                }
             }
         }
 
-        usersLoggedIn.add(myuser);
-        myuser.setIncorrectAttempt(0);
-        return myuser.getEmail() + " logged in successfully!";
+
+        usersLoggedIn.add(user);
+        myuser.setIncorrectAttempt(0); //registered user
+        return user.getEmail() + " logged in successfully!";
     }
 
     @Override
@@ -198,7 +208,7 @@ class ApplicationAuthState implements IApplicationAuthState{
 }
 
 /*
-I/P:
+I/P 1:
 1
 location4
 4
@@ -219,6 +229,60 @@ Register:1
 Register:2
 */
 
+
+/*
+I/P 2:
+2
+location1
+location2
+3
+7,user7@email.com,90559,location7
+10,user10@email.com,41853,location10
+2,user2@email.com,80573,location2
+5
+Register:1
+Register:1
+Login:0
+Login:1
+Register:2
+*/
+
+
+/*
+I/P 3:
+2
+location1
+location2
+3
+7,user7@email.com,90559,location7
+10,user10@email.com,41853,location10
+2,user2@email.com,80573,location2
+5
+Register:1
+Register:1
+Login:1
+Login:1
+Register:2
+*/
+
+
+/*
+I/P 4:
+2
+location1
+location2
+4
+7,user7@email.com,90559,location7
+10,user10@email.com,41853,location10
+2,user2@email.com,80573,location2
+10,user10@email.com,41853,location11
+5
+Register:1
+Register:1
+Login:1
+Login:3
+Register:2
+*/
 
 public class AuthenticationSolution {
 
@@ -257,40 +321,5 @@ public class AuthenticationSolution {
         }
 
         writer.close();
-
-//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-//        PrintWriter writer = new PrintWriter(System.out);
-//
-//        List<IUser> users = new ArrayList<IUser>();
-//        List<String> allowedLocations = new ArrayList<String>();
-//
-//        int allowedLocationCount = Integer.parseInt(bufferedReader.readLine().trim());
-//        for (int i = 0; i < allowedLocationCount; i++) {
-//            String a = bufferedReader.readLine().trim();
-//            allowedLocations.add(a);
-//        }
-//
-//        ApplicationAuthState authState = new ApplicationAuthState(allowedLocations);
-//
-//        int usersCount = Integer.parseInt(bufferedReader.readLine().trim());
-//        for (int i = 0; i < usersCount; i++) {
-//            String[] a = bufferedReader.readLine().trim().split(",");
-//            users.add(new User(Integer.parseInt(a[0]), a[1], a[2], a[3]));
-//        }
-//
-//        int actionCount = Integer.parseInt(bufferedReader.readLine().trim());
-//        for (int i = 0; i < actionCount; i++) {
-//            String[] a = bufferedReader.readLine().trim().split(":");
-//            int uindex = Integer.parseInt(a[1]);
-//            if (a[0].equals("Register")) {
-//                writer.println(authState.Register(users.get(uindex)));
-//            } else if (a[0].equals("Login")) {
-//                writer.println(authState.Login(users.get(uindex)));
-//            } else if (a[0].equals("Logout")) {
-//                writer.println(authState.Logout(users.get(uindex)));
-//            }
-//        }
-//
-//        writer.close();
     }
 }
